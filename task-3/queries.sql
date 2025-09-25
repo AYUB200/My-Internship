edc01805d1d3b2b92fe9f9d13d6757f4e2803465
AYUB200 <131674340+AYUB200@users.noreply.github.com>
2025-09-25 15:16:12 +0530

    Update queries.sql

--- task-3/queries.sql
+++ task-3/queries.sql
@@ -11,6 +11,18 @@ SELECT * FROM Book WHERE category = 'Fantasy';
 -- LIKE operator
 SELECT * FROM Member WHERE name LIKE 'A%';
 
+-- WHERE with AND
+SELECT * FROM Book 
+WHERE category = 'Fantasy' AND publisher = 'Bloomsbury';
+
+-- WHERE with OR
+SELECT * FROM Book 
+WHERE category = 'Fantasy' OR category = 'Dystopian';
+
+-- BETWEEN example (assume borrow_date values exist)
+SELECT * FROM Borrow 
+WHERE borrow_date BETWEEN '2025-01-01' AND '2025-12-31';
+
 -- ORDER BY
 SELECT * FROM Book ORDER BY title ASC;
 
